803660bdfbb617570d09d148442db3f1b40525b1
maggieyibo <maggiewang430@gmail.com>
2015-02-25 10:11:24 -0800

    working map, new layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
 
 			flash[:success] = "Login Successful"
 			session[:user_id] = user.id
-			redirect_to user_path(user.id)
+			redirect_to listings_path
 		else
 			flash.now[:danger] = "Unable to Login, Please try again!"
 			render :new
