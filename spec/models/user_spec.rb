02b835351f222f1e84ead5f62815da08fd38e018
gogojojo <joseph.gochua@gmail.com>
2015-02-26 16:06:33 -0800

    added some pending validations to users model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a unique email' 
+  it 'has a unique username'
+  it 'has is invalid without a password'
 end
