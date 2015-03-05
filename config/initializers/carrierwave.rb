1872e533b66ff908d553864794d7973c409362f2
maggieyibo <maggiewang430@gmail.com>
2015-03-04 21:14:23 -0800

    fixing everythiang

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
     :aws_access_key_id      => ENV['S3_KEY'],                        # required
     :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
     :region                 => ENV['S3_REGION']                  # optional, defaults to 'us-east-1'
-    # :host                   => 's3.example.com',             # optional, defaults to nil
-    # :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+    #:host                   => '',             # optional, defaults to nil
+    #:endpoint               => 's3.amazonaws.com' # optional, defaults to nil
   }
   config.fog_directory  = ENV['S3_BUCKET']                          # required (this is your bucket)
   # config.fog_public     = false                                        # optional, defaults to true
